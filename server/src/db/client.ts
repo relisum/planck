7030579b337410cf109e7b9177f4c32446a9aa08
relisum <bastrykin@epublish.ru>
2026-06-05 01:54:20 +0300

    PROD: db connect

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,8 +1,8 @@
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import path from 'path'
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const DB_PATH = path.resolve(process.cwd(), 'dev.db')
+const DB_PATH = path.resolve(process.cwd(), process.env.DB_PATH ?? 'dev.db')
 
 const adapter = new PrismaBetterSqlite3({
   url: `file:${DB_PATH}`,
